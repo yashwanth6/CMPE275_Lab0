98df6ce47826c6004a4adb2ce3b10ba8141f92be
yashwanth6 <yashu654321@gmail.com>
2022-02-06 18:26:23 -0800

    Added interface and Class

--- Lab0/src/com/lab0/Greeter.java
+++ Lab0/src/com/lab0/Greeter.java
@@ -1,5 +1,6 @@
 package com.lab0;
 
 public interface Greeter {
-
+	void setName(String name); // name of the author
+	String getGreeting();
 }
--- Lab0/src/com/lab0/HelloWorld.java
+++ Lab0/src/com/lab0/HelloWorld.java
@@ -1,5 +1,22 @@
 package com.lab0;
 
 public class HelloWorld implements Greeter {
+	
+	public String name;
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	@Override
+	public String getGreeting() {
+		// TODO Auto-generated method stub
+		return "Hello World";
+	}
 
 }
